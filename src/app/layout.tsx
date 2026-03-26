3f8a6c9c0b9117a7ad2f618fefbc88ab061e6960
Rahi-007 <linnima072@gmail.com>
2026-03-26 20:49:18 +0600

    basic commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/provider/themeProvider";
+import { Salsa, Roboto_Condensed } from "next/font/google";
+import "../style/globals.css";
+
+const salsa = Salsa({
+  variable: "--font-salsa",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const roboto = Roboto_Condensed({
+  variable: "--font-roboto-condensed",
+  subsets: ["latin"],
+  weight: "500",
+});
+
+export const metadata: Metadata = {
+  title: "Fuel Map",
+  description: "Find petrol and diesel stations across Bangladesh with FuelMap.bd. Check fuel availability, station locations, and save time on your refueling trips.",
+};
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Readonly<IProps>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${salsa.variable} ${roboto.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
